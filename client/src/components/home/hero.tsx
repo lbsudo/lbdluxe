c2e9fb629536d8e055b7c1a42724c07abc0b0497
lbsudo <lbsudo100@gmail.com>
2025-09-19 12:17:28 -0700

    server call function fixed on homepage

--- client/src/components/home/hero.tsx
+++ client/src/components/home/hero.tsx
@@ -5,7 +5,7 @@ import {useMutation} from "@tanstack/react-query";
 import {hcWithType} from "server/dist/client";
 
 // const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000
-export const SERVER_URL = import.meta.env.DEV ? "http://localhost:3000/api" : "/api";
+export const SERVER_URL = import.meta.env.DEV ? "http://localhost:3000" : "/api";
 
 const client = hcWithType(SERVER_URL);
 
