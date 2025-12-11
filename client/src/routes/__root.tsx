2578a3182a53fa79919650dca686d03b506bdc82
Lawrence Brown <lbsudo100@gmail.com>
2025-12-11 04:33:26 -0800

    added toast from sonner

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import {Toaster} from "sonner";
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
 			<Outlet />
 			<TanStackRouterDevtools />
+			<Toaster position="bottom-center" richColors={true}/>
 		</>
 	),
 });
