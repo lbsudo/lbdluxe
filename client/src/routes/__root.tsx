784b0e7a50e3656c7c8d5b21ff724898f5e851a9
Lawrence Brown <lbsudo100@gmail.com>
2026-01-03 01:15:15 -0800

    Added metadata support structure in root and index route, also changed favicon

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,11 +1,32 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import {
+  createRootRoute,
+  Outlet,
+  HeadContent,
+  Scripts,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
-	component: () => (
-		<>
-			<Outlet />
-			<TanStackRouterDevtools />
-		</>
-	),
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "LBDLUXE",
+      },
+    ],
+  }),
+  component: () => (
+    <>
+      <HeadContent />
+      <Outlet />
+      <TanStackRouterDevtools />
+      <Scripts />
+    </>
+  ),
 });
