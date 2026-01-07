fbf4ff588bb8e900732d446b21a42f306c63981a
lbsudo <lbsudo100@gmail.com>
2026-01-06 22:56:33 -0800

    api url for production changes

--- client/src/hooks/server/useHelloHono.ts
+++ client/src/hooks/server/useHelloHono.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { hcWithType } from "server/client";
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:8787";
 const client = hcWithType(SERVER_URL) as any;
 
 type ResponseType = Awaited<ReturnType<typeof client.hello.$get>>;
